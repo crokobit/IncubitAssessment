faff742ae5b11683e838ad03d2dea5faa5af98d0
ivan <crokobit@gmail.com>
2019-04-29 05:20:38 +0800

    add password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,14 @@ class User < ApplicationRecord
   include BCrypt
   validate :check_username, on: :update
   validates :email, presence: true, uniqueness: true
+  validate :password_length
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 
   before_create :set_user_name
+
+  def password_length
+    errors.add(:password, "length must greater than 8") if password.present? && password.length < 8
+  end
 
   def set_user_name
     prefix = email.split('@').first
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -3,25 +3,29 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "on creating" do
     it 'set prefix as username' do
-      User.create(email: 'fortest@gmail.com', password: 'j')
+      User.create(email: 'fortest@gmail.com', password: '12345678')
       expect(User.last.username).to eq 'fortest'
     end
-    it 'username can be smaller that 5 chars' do
-      User.create(email: 'test@gmail.com', password: 'j')
+    it 'username can be smaller than 5 chars' do
+      User.create(email: 'test@gmail.com', password: '12345678')
       expect(User.last.username).to eq 'test'
+    end
+    it 'password lenght can not be smaller than 8 chars' do
+      user = User.create(email: 'test@gmail.com', password: '1')
+      expect(user.errors.first).to eq [:password, "length must greater than 8"]
     end
   end
 
   describe "on updating" do
     it 'username can not smaller that 5 chars' do
-      user = User.create(email: 'test@gmail.com', password: 'j')
+      user = User.create(email: 'test@gmail.com', password: '12345678')
       user.update(username: 'dddd' ,password: 'j')
       expect(User.last.username).to eq 'test'
     end
     it 'pass validation with username < 5 chars, username not changed but password changed' do
-      user = User.create(email: 'test@gmail.com', password: 'j')
-      user.update(password: 'k')
-      expect(User.last.password == 'k').to be true
+      user = User.create(email: 'test@gmail.com', password: '12345678')
+      user.update(password: '23456789')
+      expect(User.last.bcrypt_password == '23456789').to be true
     end
   end
 end
